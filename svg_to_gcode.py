953800fdf813780dbf386002bb1a741d3eb43aa7
Mathéo Guilloux <matheo.guilloux@gmail.com>
2023-04-24 18:19:05 +0200

    tryc

--- svg_to_gcode.py
+++ svg_to_gcode.py
@@ -5,26 +5,25 @@ from svgpathtools import Line, CubicBezier
 
 def parse_svg(file_path):
     paths, attributes = svg2paths(file_path)
-    red_paths = [path for path, attr in zip(paths, attributes)]
-    return red_paths
+    return [path for path, attr in zip(paths, attributes)]
 
-def path_to_gcode(path, scale=1.0, power=255, bezier_steps=100):
+def path_to_gcode(path, power=255, bezier_steps=100):
     def move_to(x, y, gcode_prefix):
         return f"{gcode_prefix} X{x:.2f} Y{y:.2f}\n"
 
     gcode = ""
 
     for i, segment in enumerate(path):
-        if isinstance(segment, Line) or isinstance(segment, CubicBezier):
+        if isinstance(segment, (Line, CubicBezier)):
             if isinstance(segment, Line):
                 points = [segment.start, segment.end]
             else:
                 points = [segment.point(t / bezier_steps) for t in range(bezier_steps + 1)]
 
             for j, point in enumerate(points):
-                x, y = point.real * scale, point.imag * scale
+                x, y = point.real, point.imag
                 gcode_prefix = "G0" if i == 0 and j == 0 else "G1"
-                
+
                 if i == 0 and j == 0:
                     gcode += move_to(x, y, gcode_prefix)
                     gcode += f"M106 S{power}\n"  # Laser ON
@@ -34,27 +33,27 @@ def path_to_gcode(path, scale=1.0, power=255, bezier_steps=100):
     gcode += "M107\n"  # Laser OFF
     return gcode
 
-def svg_to_gcode(file_path, scale=1.0, power=255, pass_nbr=1, speed=90):
+def svg_to_gcode(file_path, power=255, pass_nbr=1, speed=90):
     red_paths = parse_svg(file_path)
     gcode = f"G90\nG21\nG1 F{speed}\n"  # Set to absolute positioning, millimeters, and feed rate
 
     gcode += "G28 X0 Y0\n"
     for _ in range(pass_nbr):
         for path in red_paths:
-            gcode += path_to_gcode(path, scale=scale, power=power)
+            print(path)
+            gcode += path_to_gcode(path, power=power)
     
     gcode += "G0 X0 Y0\n"  # Return to origin
     return gcode
 
 def main():
-    input_svg = 'wall1.svg'
-    output_gcode = 'wall1.gcode'
-    scale = 1.0
+    input_svg = 'base.svg'
+    output_gcode = 'base.gcode'
     power = 255
     pass_nbr = 120
     speed = 900
 
-    gcode = svg_to_gcode(input_svg, scale=scale, power=power, pass_nbr=pass_nbr, speed=speed)
+    gcode = svg_to_gcode(input_svg, power=power, pass_nbr=pass_nbr, speed=speed)
 
     with open(output_gcode, 'w') as f:
         f.write(gcode)
